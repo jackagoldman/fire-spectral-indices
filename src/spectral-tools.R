3e58004f46607508a59d46d224160af6b08cc123
jackagoldman <jandrewgoldman@gmail.com>
2024-02-18 15:38:24 -0500

    image collection functions

---
+++ src/spectral-tools.R
@@ -0,0 +1,52 @@
+
+# Create landsat 8 image collection for NBR
+ls8_indices <- function(ls_img){
+  nbr <- ls_img$normalizedDifference(c('B5', 'B7'))$float()$rename("nbr")
+  qa <- ls_img$select('pixel_qa')
+  nbr$addBands(qa)$
+    select(0,1, 'nbr', 'pixel_qa')$
+    copyProperties(ls_img, list("system:time_start"))
+  
+  return(nbr)
+  
+}
+
+# Create landsat 4-7 image collection for NBR
+ls4_7_indices <- function(ls_img){
+  nbr <- ls_img$normalizedDifference(c('B4', 'B7'))$float()$rename("nbr")
+  qa <- ls_img$select('pixel_qa')
+  nbr$addBands(qa)$
+    select(0,1, 'nbr', 'pixel_qa')$
+    copyProperties(ls_img, list("system:time_start"))
+  
+  return(nbr)
+  
+}
+
+
+# create mask for clear pixels
+lscf_mask <- function(ls_img){
+  quality <- ls_img$select('pixel_qa')
+  clear <- quality$bitwiseAnd(8)$eq(0)$ #cloud shadow
+    And(quality$bitwiseAnd(32)$eq(0))$ # cloud
+    And(quality$bitwiseAnd(4)$eq(0))$ # water
+    And(quality$bitwiseAnd(16)$eq(0)) #snow
+  ls_img <- ls_img$updateMask(clear)$select(0)$
+    copyProperties(ls_img, list("system:time_start"))
+  return(ls_img)
+}
+
+
+# function to map  around indices to get images as NBR and apply mask
+filter_ls <- function(ls_img, lsVersion){
+  if(lsVerion == "8"){
+    ls <- ls_img$map(ls8_indices)$map(lscf_mask)
+  }else{
+    ls <- ls_img$map(ls4_7_indices)$map(lscf_mask)
+  }
+ return(ls)
+}
+
+
+# Merge Landsat Collections
+ls_col <- ee$ImageCollection(ls8$merge(ls7)$merge(ls5)$merge(ls4))
