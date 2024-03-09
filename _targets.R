532c1ca2884442a7bd0c6c58a10fcdad87b576c8
jackagoldman <jandrewgoldman@gmail.com>
2024-03-09 11:42:06 -0500

    update targets pipeline

--- _targets.R
+++ _targets.R
@@ -1,11 +1,6 @@
-# Created by use_targets().
-# Follow the comments below to fill in this target script.
-# Then follow the manual to check and run the pipeline:
-#   https://books.ropensci.org/targets/walkthrough.html#inspect-the-pipeline
-
-# Load packages required to define the pipeline:
 library(targets)
-# library(tarchetypes) # Load other packages as needed.
+library(tarchetypes)
+library(rgee)
 
 # Set target options:
 tar_option_set(
@@ -25,8 +20,19 @@ ls5path <- 'LANDSAT/LT05/C01/T1_SR' # 5
 ls4path <- 'LANDSAT/LT04/C01/T1_SR' # 4
 
 
-# intialize ee
+#fire path
+firePath <- "/Users/jgoldman/Library/CloudStorage/OneDrive-UniversityofToronto/Data/qc-data/clean-data/defoliated_perimeters.shp"
+
+#Google Drive Folder
+GG_DIR <- "/Users/jgoldman/Library/CloudStorage/GoogleDrive-jandrewgoldman@gmail.com/My Drive/rgee_res"
+
+#intialize ee
 ee$Initialize()
+
+# params
+startday <- 152
+endday <- 273
+band_list <- list("preNBR", "postNBR", "rbr", "dnbr")
 
 # Replace the target list below with your own:
 list(
@@ -38,6 +44,10 @@ list(
   tar_target(name = ls_7, command = filter_ls(ls7, 8)),
   tar_target(name = ls_5, command = filter_ls(ls5, 8)),
   tar_target(name = ls_4, command = filter_ls(ls4, 8)),
+  tar_target(name = fire, command = readFire(firePath)),
+  tar_target(name = ls_col, command = merge_imageColl(ls_8, ls_7, ls_5, ls_4)),
+  tar_target(name = fire.severity.res, command = getMetrics(fire, ls_col, GG_DIR))
+  
   
   
   
